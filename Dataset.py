6112f261f0b955f3e42c8aaf695a3cf3c7696bac
Kyle Webster <kyle1234webster@gmail.com>
2020-09-17 18:55:09 -0600

    Added initial code, converting to tree methods

--- Dataset.py
+++ Dataset.py
@@ -6,6 +6,27 @@ class Dataset:
 
     def __init__(self, addr="dataset/txn_by_dept.csv"):
         """ Initialize table reading the data from the specified address"""
-        #self.table = ...
+        data = pd.read_csv(addr)
+        df = pd.DataFrame(data, columns=['POS Txn', 'ID'])
+        rows = df['POS Txn'].unique()
+        cols = df['ID'].unique()
+        binary = np.zeros((df['POS Txn'].unique().size, cols.size))
+        for ind in df.index:
+            binary[np.where(rows == df['POS Txn'][ind])[0][0]][np.where(cols == df['ID'][ind])[0][0]] = 1
+        self.binary = pd.DataFrame(binary, columns=cols, index=rows)
 
-    #def
+    def getTable(self):
+        return self.binary
+
+    def computeSupport(self, c):
+        index = 0
+        s = 0
+        for k in self.binary.index:
+            if all(self.binary.loc[k][elem] == 1 for elem in c):
+                s += 1
+        index += 1
+        return s
+
+
+
+
--- ItemMining.py
+++ ItemMining.py
@@ -8,6 +8,7 @@ class Itemset:
         self.parent = parent
         self.children = None
         self.X = itemset
+        self.support = 0
 
     def addChildren(self, children=None):
         self.children = children
@@ -18,20 +19,35 @@ class Itemset:
     def getSiblings(self):
         return self.parent.children  # ... \X
 
+    def getItemset(self):
+        return self.X
+
 
 class ItemMining:
     """Main class used for Itemset Mining. Zaki and Meira book, Chapter 8."""
 
     def __init__(self, addr, minSup=3, minConf=0.5):
         self.table = Dataset(addr)
-        self.root = []
+        self.root = Itemset()
         self.minSup = minSup
         self.minConf = minConf
 
     def runApriori(self):
-        return self.table
+        frequent = []
+        k = 1
+        c = {k: list([el] for el in self.table.columns)}
+        while c[k]:
+            support = Dataset.computeSupport(c[k], self.table)
+            newLayer = []
+            for i in range(len(c[k])):
+                if support[i] >= self.minSup:
+                    frequent.append(c[k][i])
+                    newLayer.append(c[k][i])
+            k += 1
+            c[k] = self.extendPrefixTree(newLayer)
+        return frequent
 
-    def extendPrefixTree(self):
+    def extendPrefixTree(self, c):
         return self.table
 
     def associationRules(self):
